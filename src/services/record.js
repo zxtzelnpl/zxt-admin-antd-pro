24bad30a8258c6a3f8e6fe40c7b29b38d3ccd077
was <zxt_zel_npl@163.com>
2018-06-20 09:38:54 +0800

    finish the record-service

--- src/services/record.js
+++ src/services/record.js
@@ -1,9 +1,15 @@
 import request from '../utils/request';
 
 export async function getListsNum(params){
-  return request('/admin/records/total',params);
+  return request('/admin/records/total',{
+    method:'POST',
+    body:params,
+  });
 }
 
 export async function getOnePageList(params){
-  return request('/admin/records/page',params);
+  return request('/admin/records/page',{
+    method:'POST',
+    body:params,
+  });
 }
